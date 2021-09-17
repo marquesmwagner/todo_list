c8c46afe9139607c7307da953687bfc1bbb134fe
Wagner Marques <marquesmwagner@gmail.com>
2021-09-16 23:11:57 -0300

    Update buttons

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//Dia - Mês - Ano
+let agora = new Date();
+let dia = agora.getDate();
+let mes = agora.toLocaleString('default', {month:'long'});
+let ano = agora.getFullYear();
+document.querySelector('.data-dia').innerHTML = dia;
+document.querySelector('.data-mes').innerHTML = mes;
+document.querySelector('.data-ano').innerHTML = ano;

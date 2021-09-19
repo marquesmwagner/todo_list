2aecd85ec17c3dfd803a71ac8748663ae32fb6c1
Wagner Marques <marquesmwagner@gmail.com>
2021-09-19 15:47:35 -0300

    Update css and add the js file

--- js/main.js
+++ js/main.js
@@ -6,3 +6,63 @@ let ano = agora.getFullYear();
 document.querySelector('.data-dia').innerHTML = dia;
 document.querySelector('.data-mes').innerHTML = mes;
 document.querySelector('.data-ano').innerHTML = ano;
+
+// Recebimento de dados (input e ul)
+const lista = document.querySelector('.lista-tarefas');
+const dados = document.querySelector('.adicionar-tarefa');
+
+
+// Focar na inserção de tarefas
+function focarInsercao() {
+    document.querySelector('.adicionar-tarefa').value = '';
+    document.querySelector('.adicionar-tarefa').focus();
+}
+
+// Adicionar tarefa na lista
+function adcTarefa() {
+    const novaTarefa = document.querySelector('.adicionar-tarefa').value;
+    if (novaTarefa.length == 0) {
+        alert('Você precisa digitar uma tarefa válida.');
+    } else {
+        document.querySelector('.lista-tarefas').innerHTML += 
+        '<li><input type="checkbox" id="check"><span>' + novaTarefa + 
+        '</span><button class="botao-excluir" onclick="removerTarefa(this)"><i class="fas fa-minus-square fa-lg"></i></button></li>'
+        salvarLista();
+        focarInsercao();
+    }
+}
+
+// Usar o ENTER para enviar a tarefa
+document.querySelector('.adicionar-tarefa').addEventListener('keyup', function(evento) {
+    if (evento.keyCode === 13) {
+        adcTarefa();
+        salvarLista();
+        focarInsercao();
+    }
+})
+
+// Remover tarefa da lista
+function removerTarefa(evento) {
+    let remover = confirm('Deseja remover a tarefa?');
+    if (remover) {
+        evento.parentNode.parentNode.removeChild(evento.parentNode);
+        salvarLista();
+    }
+}
+
+// Salvar a lista de tarefas em localStorage
+function salvarLista() {
+    localStorage.setItem('listaTarefas', JSON.stringify(document.querySelector('.lista-tarefas').innerHTML));
+}
+
+// Carregar lista de tarefas
+function carregarLista() {
+    let carregar = JSON.parse(localStorage.getItem('listaTarefas'));
+    if (carregar == null) {
+        return;
+    } else {
+        document.querySelector('.lista-tarefas').innerHTML = carregar;
+    }
+}
+
+carregarLista();
